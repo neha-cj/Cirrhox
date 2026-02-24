2588ba8ad7d0469c216266aa81db93ecdb79a369
Keethu-7 <keethujrchinni@gmail.com>
2026-02-24 16:21:34 +0530

    Switched to OAuth2

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -82,42 +82,6 @@ def get_me(current_user: User = Depends(get_current_user)):
     }
 
 
-
-
-# from fastapi import APIRouter, Depends, HTTPException
-# from fastapi.security import OAuth2PasswordRequestForm
-# from sqlalchemy.orm import Session
-
-# from database import get_db
-# from models_db.user_model import User
-# from schemas.user_schema import UserCreate
-# from utils.auth_utils import (
-#     hash_password,
-#     verify_password,
-#     create_access_token,
-#     get_current_user
-# )
-
-# router = APIRouter()
-
-# @router.post("/login")
-# def login(
-#     form_data: OAuth2PasswordRequestForm = Depends(),
-#     db: Session = Depends(get_db)
-# ):
-#     user = db.query(User).filter(User.email == form_data.username).first()
-
-#     if not user or not verify_password(form_data.password, user.password):
-#         raise HTTPException(status_code=401, detail="Invalid credentials")
-
-#     access_token = create_access_token(data={"sub": user.email})
-
-#     return {
-#         "access_token": access_token,
-#         "token_type": "bearer",
-#         "role": user.role
-#     }
-
 # # from fastapi import APIRouter, Depends, HTTPException
 # # from sqlalchemy.orm import Session
 # # from database import get_db
