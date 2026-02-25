8ae67f86c391b067014dfc1fd2fdc61510675a67
Neha C J <cjneha890@gmail.com>
2026-02-25 11:43:31 +0530

    Added dotenv support for environment-based DB configuration

--- backend/database.py
+++ backend/database.py
@@ -2,6 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # 👈 this loads .env file
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
